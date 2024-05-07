8442fe8f2414f7cb08ab42cf9863236fdb61041f
Guido Diego Quispe Robles <quispeguido2002@gmail.com>
2024-05-07 01:07:20 -0300

    new size mode in sprite debug mode

--- src/debugPVZ/menuDebugSprite.cpp
+++ src/debugPVZ/menuDebugSprite.cpp
@@ -7,6 +7,19 @@ bool isMainMenuActive = true;
 bool animationFound = false;
 bool rotateFound = false;
 bool rotateMode = false;
+bool sizeMode = false;
+
+void speedDebugOptions() {
+  if (padPressTimer < 20) {
+    padPressTimer++;
+
+    if (padPressTimer == 10) {
+      padSpeed = 2.0f;
+    } else if (padPressTimer == 20) {
+      padSpeed = 3.0f;
+    }
+  }
+}
 
 void subMenuSprite(Tyra::Pad& pad, Tyra::Font& font, int& entitieID) {
   if (pad.getClicked().L1 && animationFound == true) {
@@ -31,88 +44,86 @@ void subMenuSprite(Tyra::Pad& pad, Tyra::Font& font, int& entitieID) {
     playAnimation = false;
     rotateMode = false;
     rotateFound = false;
+    sizeMode = false;
   } else if (pad.getClicked().Square) {
     hideText = !hideText;
   } else if (pad.getClicked().Cross && animationFound == true) {
     playAnimation = !playAnimation;
   } else if (pad.getClicked().Triangle && rotateFound == true) {
     rotateMode = !rotateMode;
+    if (sizeMode == true) {
+      sizeMode = false;
+    }
+  } else if (pad.getClicked().L3) {
+    sizeMode = !sizeMode;
+    if (rotateMode == true) {
+      rotateMode = false;
+    }
   }
 
   if (padTimer > 0) {
     padTimer--;
   } else {
     if (menuUpOption(pad)) {
-      if (padPressTimer < 20) {
-        padPressTimer++;
-
-        if (padPressTimer == 10) {
-          padSpeed = 2.0f;
-        } else if (padPressTimer == 20) {
-          padSpeed = 3.0f;
-        }
-      }
-
-      if (rotateMode == false) {
+      speedDebugOptions();
+      if (rotateMode == false && sizeMode == false) {
         posArray[entitieID].y -= padSpeed;
-      } else {
+      } else if (sizeMode == false) {
         angles[entitieID] += padSpeed;
+      } else {
+        debugSpritesType[entitieID]->size.y -= padSpeed;
       }
 
     } else if (menuDownOption(pad)) {
-      if (padPressTimer < 20) {
-        padPressTimer++;
+      speedDebugOptions();
 
-        if (padPressTimer == 10) {
-          padSpeed = 2.0f;
-        } else if (padPressTimer == 20) {
-          padSpeed = 3.0f;
-        }
-      }
-
-      if (rotateMode == false) {
+      if (rotateMode == false && sizeMode == false) {
         posArray[entitieID].y += padSpeed;
-      } else {
+      } else if (sizeMode == false) {
         angles[entitieID] -= padSpeed;
+      } else {
+        debugSpritesType[entitieID]->size.y += padSpeed;
       }
     }
 
     if (menuLeftOption(pad)) {
-      if (padPressTimer < 20) {
-        padPressTimer++;
+      speedDebugOptions();
 
-        if (padPressTimer == 10) {
-          padSpeed = 2.0f;
-        } else if (padPressTimer == 20) {
-          padSpeed = 3.0f;
-        }
-      }
-      if (rotateMode == false) {
+      if (rotateMode == false && sizeMode == false) {
         posArray[entitieID].x -= padSpeed;
-      } else {
+      } else if (sizeMode == false) {
         angles[entitieID] -= padSpeed;
+      } else {
+        debugSpritesType[entitieID]->size.x -= padSpeed;
       }
     } else if (menuRightOption(pad)) {
-      if (padPressTimer < 20) {
-        padPressTimer++;
+      speedDebugOptions();
 
-        if (padPressTimer == 10) {
-          padSpeed = 2.0f;
-        } else if (padPressTimer == 20) {
-          padSpeed = 3.0f;
-        }
-      }
-      if (rotateMode == false) {
+      if (rotateMode == false && sizeMode == false) {
         posArray[entitieID].x += padSpeed;
-      } else {
+      } else if (sizeMode == false) {
         angles[entitieID] += padSpeed;
+      } else {
+        debugSpritesType[entitieID]->size.x += padSpeed;
       }
     }
 
     if (menuUpOption(pad) == false && menuDownOption(pad) == false &&
         menuLeftOption(pad) == false && menuRightOption(pad) == false) {
-      padPressTimer = 0;
-      padSpeed = 1;
+      if (pad.getPressed().L2 && sizeMode == true) {
+        speedDebugOptions();
+        padTimer = 10;
+        debugSpritesType[entitieID]->size.x -= padSpeed;
+        debugSpritesType[entitieID]->size.y -= padSpeed;
+      } else if (pad.getPressed().R2 && sizeMode == true) {
+        speedDebugOptions();
+        padTimer = 10;
+        debugSpritesType[entitieID]->size.x += padSpeed;
+        debugSpritesType[entitieID]->size.y += padSpeed;
+      } else {
+        padPressTimer = 0;
+        padSpeed = 1;
+      }
     }
   }
 
@@ -124,16 +135,22 @@ void subMenuSprite(Tyra::Pad& pad, Tyra::Font& font, int& entitieID) {
     if (rotateFound == true) {
       engine->font.drawText(&myFont, "PRESS A FOR Rotate", 30, 320, 16, black);
     }
-    if (rotateMode == false) {
+    if (rotateMode == false && sizeMode == false) {
       std::string position =
           "Position: " + std::to_string(posArray[entitieID].x) + ", " +
           std::to_string(posArray[entitieID].y);
 
       engine->font.drawText(&myFont, position, 30, 140, 16,
                             Tyra::Color(0, 0, 0, 128));
-    } else {
+    } else if (sizeMode == false) {
       std::string angle = "Angle: " + std::to_string(angles[entitieID]);
       engine->font.drawText(&myFont, angle, 30, 140, 16,
+                            Tyra::Color(0, 0, 0, 128));
+    } else {
+      std::string size =
+          "Size: " + std::to_string(debugSpritesType[entitieID]->size.x) +
+          ", " + std::to_string(debugSpritesType[entitieID]->size.y);
+      engine->font.drawText(&myFont, size, 30, 140, 16,
                             Tyra::Color(0, 0, 0, 128));
     }
 
@@ -167,6 +184,9 @@ void subMenuSprite(Tyra::Pad& pad, Tyra::Font& font, int& entitieID) {
       engine->font.drawText(&myFont, "PRESS X FOR PLAY/STOP ANIMATION", 30, 400,
                             16, black);
     }
+
+    engine->font.drawText(&myFont, "PRESS L3 FOR SIZE MODE", 30, 300, 16,
+                          black);
 
     engine->font.drawText(&myFont, "PRESS [ ] FOR HIDE/SHOW TEXT", 30, 380, 16,
                           black);
