0c8e8663b21cb5954bb5e2026dbb4e89ad549cdc
Guido Diego Quispe Robles <quispeguido2002@gmail.com>
2024-08-01 16:12:13 -0300

    Update arrayKey.hpp

--- inc/arrayKey.hpp
+++ inc/arrayKey.hpp
@@ -1,5 +1,6 @@
 #pragma once
-// #include <iostream>
+#include <iostream>
+#include <iterator>
 #include <vector>
 #include <tyra>
 
@@ -11,37 +12,74 @@ class ArrayKey {
   Type& read(const Key key);
   void insert(const Key key, const Type value);
   void write(const Key key, const Type value);
+  int count(const Key key);
+  void clear();
+  void erase(const Key& key);
+  Type& operator[](const Key& key);
 };
 
 template <class Key, class Type>
 Type& ArrayKey<Key, Type>::read(const Key key) {
-  int index = -1;
   for (unsigned int i = 0; i < first.size(); i++) {
     if (first[i] == key) {
-      index = i;
+      return second[i];
     }
   }
-  TYRA_ASSERT(index != -1, "ERROR SEARCHING KEY, KEY NOT FOUNDED:", key);
-  return second[index];
+  TYRA_ASSERT(!(true == true), "ERROR SEARCHING KEY, KEY NOT FOUNDED:", key);
+  return *second.end();
 }
 
 template <class Key, class Type>
 void ArrayKey<Key, Type>::insert(const Key key, const Type value) {
   first.push_back(key);
   second.push_back(value);
-  // std::cout << "key: " << key << " value: " << value << std::endl;
-  // std::cout << "value ptr: " << &second[0] << std::endl;
 }
 
 template <class Key, class Type>
 void ArrayKey<Key, Type>::write(const Key key, const Type value) {
-  int index = -1;
   for (unsigned int i = 0; i < first.size(); i++) {
     if (first[i] == key) {
-      index = i;
+      second[i] = value;
     }
   }
 
-  TYRA_ASSERT(index != -1, "ERROR SEARCHING KEY, KEY NOT FOUNDED", key);
-  second[index] = value;
+  TYRA_ASSERT(!(true == true), "ERROR SEARCHING KEY, KEY NOT FOUNDED", key);
+}
+
+template <class Key, class Type>
+int ArrayKey<Key, Type>::count(const Key key) {
+  for (unsigned int i = 0; i < first.size(); i++) {
+    if (first[i] == key) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+template <class Key, class Type>
+Type& ArrayKey<Key, Type>::operator[](const Key& key) {
+  for (unsigned int i = 0; i < first.size(); i++) {
+    if (first[i] == key) {
+      return second[i];
+    }
+  }
+  TYRA_ASSERT(!(true == true), "ERROR SEARCHING KEY, KEY NOT FOUNDED:", key);
+  return *second.end();
+}
+
+template <class Key, class Type>
+void ArrayKey<Key, Type>::clear() {
+  first.clear();
+  second.clear();
+}
+
+template <class Key, class Type>
+void ArrayKey<Key, Type>::erase(const Key& key) {
+  for (unsigned int i = 0; i < first.size(); i++) {
+    if (first[i] == key) {
+      first.erase(first.begin() + i);
+      second.erase(second.begin() + i);
+      break;
+    }
+  }
 }
