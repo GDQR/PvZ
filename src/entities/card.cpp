43a05be7dbe258e28598ece503e1c0e145fbf2c3
Guido Diego Quispe Robles <quispeguido2002@gmail.com>
2024-08-25 01:00:07 -0300

    Update card.cpp

--- src/entities/card.cpp
+++ src/entities/card.cpp
@@ -1,11 +1,12 @@
 #include "systems.hpp"
 
 void createCardPeashotter(std::vector<int>& plantID, const Tyra::Vec2 pos) {
-  plantID.push_back(Entities::newID());
   int entityID;
   int animID;
+
+  plantID.push_back(Entities::newID());
   posArray.insert(plantID[0], pos);
-  
+
   for (unsigned int i = 0;
        i < m_animID[enumAnimName::PeashooterSingleAnimName].size(); i++) {
     entityID = Entities::newID();
@@ -19,7 +20,120 @@ void createCardPeashotter(std::vector<int>& plantID, const Tyra::Vec2 pos) {
   }
 }
 
-void createCard(const Plant_State_enum typePlant, const Tyra::Vec2 pos, const bool isVersusMode) {
+void createCardSunFlower(std::vector<int>& plantID, const Tyra::Vec2 pos) {
+  int entityID;
+  int animID;
+
+  plantID.push_back(Entities::newID());
+  posArray.insert(plantID[0], pos);
+  for (unsigned int i = 0; i < m_animID[enumAnimName::SunFlowerAnimName].size();
+       i++) {
+    entityID = Entities::newID();
+    plantID.push_back(entityID);
+    animID = m_animID[enumAnimName::SunFlowerAnimName][i];
+    newFatherID(&plantID[0], &entityID);
+    animationDataArray[animID].loadAnimation(entityID, animID, 8, 8);
+  }
+}
+
+void createCardCherryBomb(std::vector<int>& plantID, const Tyra::Vec2 pos) {
+  int entityID;
+  int animID;
+
+  plantID.push_back(Entities::newID());
+  posArray.insert(plantID[0], pos);
+  for (unsigned int i = 0;
+       i < m_animID[enumAnimName::CherryBombAnimName].size(); i++) {
+    entityID = Entities::newID();
+    plantID.push_back(entityID);
+    animID = m_animID[enumAnimName::CherryBombAnimName][i];
+    newFatherID(&plantID[0], &entityID);
+    animationDataArray[animID].loadAnimation(entityID, animID, 1, 1);
+  }
+}
+
+void createCardWallnut(std::vector<int>& plantID, const Tyra::Vec2 pos) {
+  int entityID;
+  int animID;
+
+  plantID.push_back(Entities::newID());
+  posArray.insert(plantID[0], pos);
+  for (unsigned int i = 0; i < m_animID[enumAnimName::WallnutAnimName].size();
+       i++) {
+    entityID = Entities::newID();
+    plantID.push_back(entityID);
+    animID = m_animID[enumAnimName::WallnutAnimName][i];
+    newFatherID(&plantID[0], &entityID);
+    animationDataArray[animID].loadAnimation(entityID, animID, 1, 1);
+  }
+}
+
+void createCardPotatoMine(std::vector<int>& plantID, const Tyra::Vec2 pos) {
+  int entityID;
+  int animID;
+
+  plantID.push_back(Entities::newID());
+  posArray.insert(plantID[0], pos);
+  for (unsigned int i = 0;
+       i < m_animID[enumAnimName::PotatoMineAnimName].size(); i++) {
+    entityID = Entities::newID();
+    plantID.push_back(entityID);
+    animID = m_animID[enumAnimName::PotatoMineAnimName][i];
+    newFatherID(&plantID[0], &entityID);
+    animationDataArray[animID].loadAnimation(entityID, animID, 31, 31);
+  }
+}
+
+void createCardSnowPea(std::vector<int>& plantID, const Tyra::Vec2 pos) {
+  int entityID;
+  int animID;
+
+  plantID.push_back(Entities::newID());
+  posArray.insert(plantID[0], pos);
+  for (unsigned int i = 0; i < m_animID[enumAnimName::SnowPeaAnimName].size();
+       i++) {
+    entityID = Entities::newID();
+    plantID.push_back(entityID);
+    animID = m_animID[enumAnimName::SnowPeaAnimName][i];
+    newFatherID(&plantID[0], &entityID);
+    animationDataArray[animID].loadAnimation(entityID, animID, 80, 80);
+  }
+}
+
+void createCardChomper(std::vector<int>& plantID, const Tyra::Vec2 pos) {
+  int entityID;
+  int animID;
+
+  plantID.push_back(Entities::newID());
+  posArray.insert(plantID[0], pos);
+  for (unsigned int i = 0; i < m_animID[enumAnimName::ChomperAnimName].size();
+       i++) {
+    entityID = Entities::newID();
+    plantID.push_back(entityID);
+    animID = m_animID[enumAnimName::ChomperAnimName][i];
+    newFatherID(&plantID[0], &entityID);
+    animationDataArray[animID].loadAnimation(entityID, animID, 1, 1);
+  }
+}
+
+void createCardRepeater(std::vector<int>& plantID, const Tyra::Vec2 pos) {
+  int entityID;
+  int animID;
+
+  plantID.push_back(Entities::newID());
+  posArray.insert(plantID[0], pos);
+  for (unsigned int i = 0;
+       i < m_animID[enumAnimName::PeaShooterAnimName].size(); i++) {
+    entityID = Entities::newID();
+    plantID.push_back(entityID);
+    animID = m_animID[enumAnimName::PeaShooterAnimName][i];
+    newFatherID(&plantID[0], &entityID);
+    animationDataArray[animID].loadAnimation(entityID, animID, 80, 80);
+  }
+}
+
+void createCard(const Plant_State_enum typePlant, const Tyra::Vec2 pos,
+                const bool isVersusMode) {
   Card card;
   card.seed = Entities::newID();
   card.seedShadow = Entities::newID();
@@ -33,6 +147,10 @@ void createCard(const Plant_State_enum typePlant, const Tyra::Vec2 pos, const bo
   createTexture(card.seedShadow, "UI/Seeds.png");
   spriteArray[card.seedShadow].color = Tyra::Color(0.0F, 0.0F, 0.0F, 60.0F);
 
+  // TODO: hacer que el seedshadowtimer aparezca despues del "start set plant"
+  // cuando el startwitoutwait sea verdadero
+  // y crear un efecto para cuando el seed shadow (lo de arriba) se va porque el
+  // costo de soles es mayor
 
   createSprite(card.seedShadowTimer, Tyra::MODE_REPEAT, pos, Vec2(50, 70));
   createTexture(card.seedShadowTimer, "UI/Seeds.png");
@@ -50,9 +168,23 @@ void createCard(const Plant_State_enum typePlant, const Tyra::Vec2 pos, const bo
   card.plant = typePlant;
 
   card.cost = getPlantCost(typePlant);
-  
-  if(typePlant == Plant_State_enum::PeaShotter){
+
+  if (typePlant == Plant_State_enum::PeaShotter) {
     createCardPeashotter(card.plantID, Vec2(pos.x, pos.y));
+  } else if (typePlant == Plant_State_enum::SunFlower) {
+    createCardSunFlower(card.plantID, Vec2(pos.x, pos.y));
+  } else if (typePlant == Plant_State_enum::CherryBomb) {
+    createCardCherryBomb(card.plantID, Vec2(pos.x, pos.y));
+  } else if (typePlant == Plant_State_enum::Wallnut) {
+    createCardWallnut(card.plantID, Vec2(pos.x, pos.y));
+  } else if (typePlant == Plant_State_enum::PotatoMine) {
+    createCardPotatoMine(card.plantID, Vec2(pos.x, pos.y));
+  } else if (typePlant == Plant_State_enum::SnowPea) {
+    createCardSnowPea(card.plantID, Vec2(pos.x, pos.y));
+  } else if (typePlant == Plant_State_enum::Chomper) {
+    createCardChomper(card.plantID, Vec2(pos.x, pos.y));
+  } else if (typePlant == Plant_State_enum::Repeater) {
+    createCardRepeater(card.plantID, Vec2(pos.x, pos.y));
   }
 
   cards.push_back(card);
